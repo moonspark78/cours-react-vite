e8adfd2cd4fe03e9a5927bc656357e8d0c6f9d77
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-28 17:54:31 +0200

    part compoment  :art:

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <h2>VOici un composant dans un autre fichier</h2>
       <p>Mon age est {age} ans</p>
       {
-        age > 50 ? <h3>Vous êtes vieux !</h3> : <h3>Vous êtes jeune !</h3> 
+        age > 50 ? <h3>Vous êtes vieux !</h3> : <h3>Vous êtes jeunes !</h3> 
       }
     </div>
   )
