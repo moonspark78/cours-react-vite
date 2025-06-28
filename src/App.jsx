408ffb5dadef0a18cf5e7d816bb3668afcfa0518
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-28 16:43:39 +0200

    part compoment  :art:

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>Cours ReactJS avec vite</h1>
+      <h2>VOici un composant dans un autre fichier</h2>
     </div>
   )
 }
