5da9bb7a03e3951dfb7268a0a60b874856e7fe6c
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-29 11:57:20 +0200

    working on the compteur  :art:

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
+import Compteur from './components/Compteur';
 
 function App() {
   const age = 20;
   return (
     <div className='App'>
       <h2>Un composant Simple</h2>
+      <br/>
+      <Compteur/>
     </div>
   )
 }
---
+++ src/components/Compteur.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Compteur = () => {
+  return (
+    <div>
+        <h1>Bonjours les amis </h1>
+    </div>
+  )
+}
+
+export default Compteur
