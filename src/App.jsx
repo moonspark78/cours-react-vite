b173587613f7e7b29786cb5b3b623add7c4de244
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-29 11:49:14 +0200

    part compomen  :art:

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,8 @@ import './App.css'
 function App() {
   const age = 20;
   return (
-    <div style={{backgroundColor: "red", width: "100%", height: "100vh"}}>
-      <h2>VOici un composant dans un autre fichier</h2>
-      <p>Mon age est {age} ans</p>
-      {
-        age > 50 ? <h3>Vous êtes vieux !</h3> : <h3>Vous êtes jeunes !</h3> 
-      }
+    <div className='App'>
+      <h2>Un composant Simple</h2>
     </div>
   )
 }
