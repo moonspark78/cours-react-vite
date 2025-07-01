b2239efbbc7b973e549ef7ef0639c23d3cf46c7e
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-07-01 21:14:51 +0200

    create pOST Components :art:

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import Compteur from './components/Compteur';
+import Navbar from './components/Navbar';
 
 function App() {
   const age = 20;
   const nom = "Drcmind";
   return (
     <div className='App'>
-      
+      <Navbar/>
     </div>
   )
 }
