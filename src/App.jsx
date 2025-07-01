3d2a6705b6243dfbab06db160691e0d3c378e392
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-07-01 21:22:37 +0200

    working  in Prices Sections  :art:

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,30 @@ import Navbar from './components/Navbar';
 import Post from './components/Post';
 
 function App() {
-  const age = 20;
-  const nom = "Drcmind";
+  const [posts , setPosts] = useState([
+    {
+      id:1,
+      titre: "Mon premier post",
+      description: "Ceci est la description de mon premier post",
+      liker: false
+    },{
+      id:2,
+      titre: "Mon deuxième post",
+      description: "Ceci est la description de mon deuxième post",
+      liker: false  
+    },
+    {
+      id:3,
+      titre: "Mon troisième post",
+      description: "Ceci est la description de mon troisième post",
+      liker: false
+    },{
+      id:4,
+      titre: "Mon quatrième post",
+      description: "Ceci est la description de mon quatrième post",
+      liker: false
+    }
+  ]);
   return (
     <div className='App'>
       <Navbar/>
