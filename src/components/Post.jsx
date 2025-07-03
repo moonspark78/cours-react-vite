b44ac3bac071741de044ded20960f28732d80a8d
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-07-03 18:44:28 +0200

    working  in Post  Section  :art:

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -11,7 +11,7 @@ const Post = ({data}) => {
           <h2>{data.titre}</h2>
           <p>{data.description}</p>
         </div>
-        <div>
+        <div className='btn'>
           <button>Liker</button>
           <button className='delete'>Effacer</button>
         </div>
