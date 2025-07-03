03f2fee2b062c011d4a1b3fb9ee8c79e8c425b91
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-07-03 20:01:16 +0200

    working  in Post  Section  :art:

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-        <h3>Voici la bar de navigation</h3>
+    <div className='entete'>
+        <h1 style={{textDecoration: 'underline'}}>Les Post liker sont : 10</h1>
     </div>
   )
 }
