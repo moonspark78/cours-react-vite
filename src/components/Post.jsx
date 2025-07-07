77674f34776cd6f434e9d814678a7f0f0d30765a
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-07-07 20:22:52 +0200

    working  in Price  Section  :art:

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -12,7 +12,7 @@ const Post = ({data, liker}) => {
           <p>{data.description}</p>
         </div>
         <div className='btn'>
-          <button onClick={() => liker(data)}>{"Likers"}</button>
+          <button onClick={() => liker(data)}>{data.liker ? "Likers"}</button>
           <button className='delete'>Effacer</button>
         </div>
     </div>
